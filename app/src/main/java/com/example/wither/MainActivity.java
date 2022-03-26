4ed6ab36ab9e89e41fc2afcf3c20c1fd6d8a1472
hyeonjae <75851037+myhyun0002@users.noreply.github.com>
2022-03-27 00:39:11 +0900

    bottom button + naver map api 연동

---
+++ app/src/main/java/com/example/wither/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.wither;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private HomeFragment homeFragment;
+    private ChattingFragment chattingFragment;
+    private UserFragment userFragment;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuitem) {
+                int itemId = menuitem.getItemId();
+                if (itemId == R.id.action_home) {
+                    setFrag(0);
+                } else if (itemId == R.id.action_chat) {
+                    setFrag(1);
+                } else if (itemId == R.id.action_user) {
+                    setFrag(2);
+                }
+                return true;
+            }
+        });
+
+        homeFragment = new HomeFragment();
+        chattingFragment = new ChattingFragment();
+        userFragment = new UserFragment();
+
+        setFrag(0); // 첫 fragment 화면을 homeFragment로 한다.
+
+    }
+        // 프래그먼트 교체가 일어나는 실행문이다.
+    private void setFrag(int n){
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        switch(n){
+            case 0:
+                fragmentTransaction.replace(R.id.main_frame,homeFragment);
+                fragmentTransaction.commit();
+                break;
+
+            case 1:
+                fragmentTransaction.replace(R.id.main_frame,chattingFragment);
+                fragmentTransaction.commit();
+                break;
+            case 2:
+                fragmentTransaction.replace(R.id.main_frame,userFragment);
+                fragmentTransaction.commit();
+                break;
+        }
+    }
+}
