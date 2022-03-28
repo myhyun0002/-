0b497a7551b727db1c6fc157489a897a27fa8c46
hyeonjae <75851037+myhyun0002@users.noreply.github.com>
2022-03-29 00:11:56 +0900

    현재 위치 지도 마커 표시

--- app/src/main/java/com/example/wither/HomeFragment.java
+++ app/src/main/java/com/example/wither/HomeFragment.java
@@ -1,61 +1,89 @@
 package com.example.wither;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class HomeFragment extends Fragment {
+import java.security.Permission;
+import java.util.Objects;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link HomeFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class HomeFragment extends Fragment implements OnMapReadyCallback{
+
+        // 지도에 마커 표시를 위한 코드
+
+    private static final int PERMISSION_REQUEST_CODE  = 100;
+    private static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private FusedLocationSource mLocationSource;
+    private NaverMap mNaverMap;
+
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
 
 
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
+//
+
+//    public static HomeFragment newInstance(String param1, String param2) {
+//        HomeFragment fragment = new HomeFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+                // 밑에는 지도 마커 표시를 위한 코드다
+        FragmentManager fm = getParentFragmentManager();
+        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
+        if(mapFragment == null){
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_fragment,mapFragment).commit();
         }
+
+
+        mapFragment.getMapAsync(this);
+        mLocationSource = new FusedLocationSource(this,PERMISSION_REQUEST_CODE);
     }
 
     @Override
@@ -63,5 +91,39 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
+
+        // 지도 마커 표시를 위한 메서드
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap) {
+
+        // 지도 상에 마커 표시
+        Marker marker = new Marker();
+        marker.setPosition(new LatLng(37.5670135,126.9783740));
+
+
+        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+        mNaverMap = naverMap;
+        mNaverMap.setLocationSource(mLocationSource);
+
+        marker.setIcon(OverlayImage.fromResource(R.drawable.ic_home));
+        this.requestPermissions(PERMISSIONS, PERMISSION_REQUEST_CODE);
+
+
+    }
+
+        @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+            }
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/wither/MainActivity.java
+++ app/src/main/java/com/example/wither/MainActivity.java
@@ -3,16 +3,27 @@ package com.example.wither;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.util.FusedLocationSource;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     // 여기 코드들은 하단 버튼을 눌렀을 때 그에 맞는 페이지를 보여주기 위한 코드들입니다.
 
@@ -23,11 +34,18 @@ public class MainActivity extends AppCompatActivity {
     private ChattingFragment chattingFragment;
     private UserFragment userFragment;
 
+
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+        //밑에는 fragment 간 교체를 위한 코드다
         bottomNavigationView = findViewById(R.id.bottomNavi);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -51,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
         setFrag(0); // 첫 fragment 화면을 homeFragment로 한다.
 
     }
+
+
         // 프래그먼트 교체가 일어나는 실행문이다.
     private void setFrag(int n){
         fragmentManager = getSupportFragmentManager();
@@ -71,4 +91,33 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+
+//    // 지도 마커 표시를 위한 메서드
+//    @Override
+//    public void onMapReady(@NonNull NaverMap naverMap) {
+//        Log.d(TAG,"onMapReady");
+//
+//        // 지도 상에 마커 표시
+//        Marker marker = new Marker();
+//        marker.setPosition(new LatLng(37.5670135,126.9783740));
+//
+//        // NaverMap 객체 받아서 NaverMap 객체에 위치 소스 지정
+//        mNaverMap = naverMap;
+//        mNaverMap.setLocationSource(mLocationSource);
+//
+//        ActivityCompat.requestPermissions(this,PERMISSIONS,PERMISSION_REQUEST_CODE);
+//    }
+//
+//
+//    // 지도 마커 표시를 위한 메서드
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//
+//        if(requestCode == PERMISSION_REQUEST_CODE){
+//            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                mNaverMap.setLocationTrackingMode(LocationTrackingMode.None);
+//            }
+//        }
+//    }
 }
