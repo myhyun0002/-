4e2636fd9da3d62853bf1dbdd2c9cdba55a45e84
hyeonjae <75851037+myhyun0002@users.noreply.github.com>
2022-03-30 22:51:56 +0900

    목록 버튼 + 플러스 버튼 수정

--- app/src/main/java/com/example/wither/HomeFragment.java
+++ app/src/main/java/com/example/wither/HomeFragment.java
@@ -2,8 +2,13 @@ package com.example.wither;
 
 import android.Manifest;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,9 +17,11 @@ import androidx.fragment.app.Fragment;
 
 import androidx.fragment.app.FragmentManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 
 import com.naver.maps.geometry.LatLng;
@@ -29,13 +36,18 @@ import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Marker;
 
 import com.naver.maps.map.util.FusedLocationSource;
+import com.naver.maps.map.widget.ZoomControlView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
-public class HomeFragment extends Fragment implements OnMapReadyCallback{
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
-        // 지도에 마커 표시를 위한 코드
+    // 지도에 마커 표시를 위한 코드
 
-    private static final int PERMISSION_REQUEST_CODE  = 100;
+    private static final int PERMISSION_REQUEST_CODE = 100;
     private static final String[] PERMISSIONS = {
             Manifest.permission.ACCESS_COARSE_LOCATION,
             Manifest.permission.ACCESS_FINE_LOCATION
@@ -54,15 +66,6 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        FragmentManager fm = getParentFragmentManager();
-        MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map_fragment);
-        if(mapFragment == null){
-            mapFragment = MapFragment.newInstance();
-            fm.beginTransaction().add(R.id.map_fragment,mapFragment).commit();
-        }
-
-        mapFragment.getMapAsync(this);
-        mLocationSource = new FusedLocationSource(this,PERMISSION_REQUEST_CODE);
 
     }
 
@@ -71,13 +74,26 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        Marker marker = new Marker();
-        marker.setPosition(new LatLng(34,102));
+        FragmentManager fm = getParentFragmentManager();
+
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map_fragment);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map_fragment, mapFragment).commit();
+        }
+        mapFragment.onDestroy();
+        mapFragment.getMapAsync(this);
+
+        mLocationSource = new FusedLocationSource(this, PERMISSION_REQUEST_CODE);
+
+
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
 
         // 다른 탭으로 이동 후에 다시 돌아와도 지도 초기화 안되게 하는 코드
         MapView mapView;
-        mapView = (MapView)view.findViewById(R.id.map_fragment);
+        mapView = (MapView) view.findViewById(R.id.map_fragment);
         mapView.onCreate(savedInstanceState);
         mapView.onResume();
         mapView.getMapAsync(this); // 비동기적 방식으로 구글 맵 실행
@@ -106,6 +122,15 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         mNaverMap = naverMap;
         mNaverMap.setLocationSource(mLocationSource);
 
+        Marker marker = new Marker();
+//        GpsTracker gpsTracker = new GpsTracker(getActivity());
+//        double latitude = gpsTracker.getLatitude(); // 위도
+//        double longitude = gpsTracker.getLongitude(); //경도
+
+        marker.setPosition(new LatLng(32, 102));
+        marker.setVisible(false);
+
+
         this.requestPermissions(PERMISSIONS, PERMISSION_REQUEST_CODE);
 
         // 현위치 버튼 , zoom버튼 생성
@@ -114,9 +139,11 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         uiSettings.setLocationButtonEnabled(true);
         uiSettings.setZoomControlEnabled(false);
 
-        // 네이버 로고 위치 지정
-        //uiSettings.setLogoGravity(getView().getTop());
-        //uiSettings.setLogoMargin(940,30,30,0);
+
+
+//         네이버 로고 위치 지정
+//        uiSettings.setLogoGravity(getView().getTop());
+//        uiSettings.setLogoMargin(940,30,30,0);
 
         //기울임 틸트 비율 비활성화
         uiSettings.setTiltGesturesEnabled(false);
@@ -124,14 +151,53 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
         uiSettings.setScaleBarEnabled(false);
     }
 
-        @Override
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == PERMISSION_REQUEST_CODE){
-            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                mNaverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mNaverMap.setLocationTrackingMode(LocationTrackingMode.NoFollow);
             }
         }
     }
+
+//    public String getCurrentAddress(double latitude, double longitude) {
+//
+//        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+//        List<Address> addresses;
+//
+//        try {
+//            addresses = geocoder.getFromLocation(
+//                    latitude, longitude, 100);
+//        } catch (IOException ioException) {
+//            //네트워크 문제
+//            showDialogForLocationServiceSetting();
+//            return "지오코더 서비스 사용불가";
+//        }
+//
+//
+//    }
+//
+//    private void showDialogForLocationServiceSetting() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        builder.setTitle("위치 서비스 비활성화");
+//        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n" + "위치 설정을 수정하실래요?");
+//        builder.setCancelable(true);
+//        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int id) {
+//                Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+//                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+//            }
+//        });
+//        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int id) {
+//                dialog.cancel();
+//            }
+//        });
+//        builder.create().show();
+//    }
+
 }
