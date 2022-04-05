2273195e8f237e728622b9680063636c94af8cfd
hyeonjae <75851037+myhyun0002@users.noreply.github.com>
2022-04-06 07:51:27 +0900

    모임 생성 버튼 이벤트 초안2

--- app/src/main/java/com/example/wither/HomeFloatingFragment.java
+++ app/src/main/java/com/example/wither/HomeFloatingFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
@@ -30,4 +32,17 @@ public class HomeFloatingFragment extends Fragment {
         return view;
 
     }
+//    public void showDatePicker(View view) {
+//        DialogFragment newFragment = new DatePickerFragment();
+//        newFragment.show(view.getSupportFragmentManager(),"datePicker");
+//    }
+//
+//    public void processDatePickerResult(int year, int month, int day){
+//        String month_string = Integer.toString(month+1);
+//        String day_string = Integer.toString(day);
+//        String year_string = Integer.toString(year);
+//        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+//
+//        Toast.makeText(this,"Date: "+dateMessage,Toast.LENGTH_SHORT).show();
+//    }
 }
