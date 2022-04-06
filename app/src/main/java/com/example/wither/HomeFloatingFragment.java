2408f451049d84354463aa814ecd27218d8da6ea
hyeonjae <75851037+myhyun0002@users.noreply.github.com>
2022-04-06 20:47:20 +0900

    날짜 선택 버튼 구현

--- app/src/main/java/com/example/wither/HomeFloatingFragment.java
+++ app/src/main/java/com/example/wither/HomeFloatingFragment.java
@@ -1,18 +1,43 @@
 package com.example.wither;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class HomeFloatingFragment extends Fragment {
+
+    // fragment에서 context를 쓸려면 getActivity()
+
+    TextView dateText;
+
+    DatePickerDialog datePickerDialog;
+    Calendar calendar = Calendar.getInstance();
+    int pYear = calendar.get(Calendar.YEAR); //년
+    int pMonth = calendar.get(Calendar.MONTH);//월
+    int pDay = calendar.get(Calendar.DAY_OF_MONTH);//일
+
 
     public HomeFloatingFragment() {
 
@@ -29,20 +54,49 @@ public class HomeFloatingFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_home_floatingbtn, container, false);
 
+        // 날짜 선택 버튼
+        dateText = view.findViewById(R.id.date_picker_text);
+        Button button = view.findViewById(R.id.date);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDate();
+            }
+        });
+
         return view;
 
     }
-//    public void showDatePicker(View view) {
-//        DialogFragment newFragment = new DatePickerFragment();
-//        newFragment.show(view.getSupportFragmentManager(),"datePicker");
-//    }
+    void showDate() {
+        //오늘 날짜(년,월,일) 변수에 담기
+
+        datePickerDialog = new DatePickerDialog(getActivity(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+
+                        //1월은 0부터 시작하기 때문에 +1을 해준다.
+                        month = month + 1;
+                        String date = year + "/" + month + "/" + day;
+
+                        dateText.setText(date);
+                    }
+                }, pYear, pMonth, pDay);
+        datePickerDialog.show();
+    }
+
+    // 시간 설정
+//    void showTime() {
+//        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+//            @Override
+//            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+//                h = hourOfDay;
+//                mi = minute;
 //
-//    public void processDatePickerResult(int year, int month, int day){
-//        String month_string = Integer.toString(month+1);
-//        String day_string = Integer.toString(day);
-//        String year_string = Integer.toString(year);
-//        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+//            }
+//        }, 21, 12, true);
 //
-//        Toast.makeText(this,"Date: "+dateMessage,Toast.LENGTH_SHORT).show();
+//        timePickerDialog.setMessage("메시지");
+//        timePickerDialog.show();
 //    }
 }
